237a8c0665ce8c94af0b0b8ae0afa3b876515891
Alicia Parra <alicia@redparra.com>
2022-03-26 21:39:11 +0100

    Add styles ar box-models and selectors views

--- lit-css-practice/src/views/BoxModels1.js
+++ lit-css-practice/src/views/BoxModels1.js
@@ -1,4 +1,4 @@
-import { LitElement, html, css } from 'lit-element';
+import { LitElement, html, css } from "lit-element";
 
 export class BoxModels1 extends LitElement {
   static get styles() {
@@ -7,15 +7,136 @@ export class BoxModels1 extends LitElement {
         text-align: center;
         margin: 100px;
       }
+
+      .basic-section {
+        margin: 20px;
+        background-color: #a69c9ccf;
+        padding: 20px;
+      }
+
+      .basic-section__text {
+        background-color: #f6cb9f;
+        margin: 40px;
+      }
+
+      .basic-section__text--border {
+        border: 10px dotted red;
+      }
+
+      .basic-section__text--outline {
+        outline: 10px dotted blue;
+      }
+
+      .basic-section__text--shadow {
+        box-shadow: 60px -10px teal;
+      }
+
+      #sub-section-2 {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+      }
+
+      .section__box--fixed {
+        width: 100px;
+        height: 100px;
+        border: 2px solid black;
+      }
+
+      .section__box--intrinsic {
+        width: min-content;
+        height: min-content;
+        border: solid 1px black;
+      }
+
+      #section__box--scroll-overflow {
+        overflow: scroll;
+      }
+
+      #section__box--hidden-overflow {
+        overflow: hidden;
+      }
+
+      .big-text {
+        font-size: 30px;
+      }
+
+      #sub-section-3{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
+      .sub-section-3__content-box{
+        
+        box-sizing: content-box;
+        background-color: pink;
+        width: 200px;
+        padding: 10px;
+        border: 5px solid magenta;
+      }
+
+      .sub-section-3__border-box{
+        box-sizing: border-box;
+        background-color: pink;
+        width: 200px;
+        padding: 10px;
+        border: 5px solid magenta;
+      }
     `;
   }
 
   render() {
     return html`
       <div class="text-block">
-        <h3>Chapter 1. Box models</h3>
+        <h1>Chapter 1. Box models</h1>
+        <section class="basic-section">
+          <h2>Border vs Outline vs BoxShadow</h2>
+          <p class=" basic-section__text basic-section__text--border">
+            I have a border
+          </p>
+          <p class="basic-section__text basic-section__text--outline">
+            I have an outline
+          </p>
+          <p class="basic-section__text basic-section__text--shadow">
+            I have a box-shadow
+          </p>
+        </section>
+        <section class="basic-section ">
+          <h2>Fixed size vs scrollable vs hidden vs min content</h2>
+          <div id="sub-section-2">
+            <div class="section__box--fixed">
+              <p class="big-text">I can get out of the box</p>
+            </div>
+            <div
+              class="section__box--fixed "
+              id="section__box--scroll-overflow"
+            >
+              <p class="big-text">My content is scrollable</p>
+            </div>
+            <div
+              class="section__box--fixed "
+              id="section__box--hidden-overflow"
+            >
+              <p class="big-text">I hide my overflow content</p>
+            </div>
+            <div class="section__box--intrinsic">
+              <p class="big-text">I adapt the box intrinsic</p>
+            </div>
+          </div>
+        </section>
+        <section class="basic-section ">
+          <h2>Box sizing</h2>
+          <div id="sub-section-3">
+            <div class="sub-section-3__content-box">
+              <p>Content box - 200px + padding + border = 230px</p>
+            </div>
+            <div>
+              <p class="sub-section-3__border-box">Border box - 200 px pushing padding and border = 200px</p>
+            </div>
+          </div>
+        </section>
       </div>
     `;
   }
 }
-customElements.define('box-models-1', BoxModels1);
+customElements.define("box-models-1", BoxModels1);
--- lit-css-practice/src/views/Selectors2.js
+++ lit-css-practice/src/views/Selectors2.js
@@ -1,4 +1,4 @@
-import { LitElement, html, css } from 'lit-element';
+import { LitElement, html, css } from "lit-element";
 
 export class Selectors2 extends LitElement {
   static get styles() {
@@ -7,6 +7,54 @@ export class Selectors2 extends LitElement {
         text-align: center;
         margin: 100px;
       }
+
+      .basic-section {
+        margin: 20px;
+        background-color: #a69c9ccf;
+        padding: 20px;
+      }
+
+      #sub-section-1 {
+        display: flex;
+        justify-content: space-around;
+      }
+
+      [data-type="blue"] {
+        color: blue;
+      }
+
+      [data-type="red"] {
+        color: red;
+      }
+
+      [data-type] {
+        width: min-content;
+        padding: 10px;
+      }
+
+      [background-id] {
+        width: min-content;
+        padding: 10px;
+        background-color: grey;
+      }
+
+      [href^="https"] {
+        color: green;
+      }
+
+      span:nth-child(even) {
+        background: mediumseagreen;
+      }
+
+      em:hover {
+        background: yellowgreen;
+        cursor: pointer;
+      }
+
+      .pseudo-element::before,
+      .pseudo-element::after {
+        content: "*";
+      }
     `;
   }
 
@@ -14,9 +62,47 @@ export class Selectors2 extends LitElement {
     return html`
       <div class="text-block">
         <h3>Chapter 2. Selectors</h3>
-        <p>Content here</p>
+        <section class="basic-section">
+          <h2>Atributtes selector</h2>
+          <div id="sub-section-1">
+            <p data-type="blue" background-id="1">
+              I have a blue data-type and background-id (Change color and
+              background)
+            </p>
+            <p data-type="red">
+              I do not have background-id but I have an red data-type (Change
+              only color)
+            </p>
+            <p background-id="3">
+              I do not have data type attribute but I have a background-id
+              (Change only background)
+            </p>
+
+            <a href="https://google.com"
+              >This link contains https so I paint it green</a
+            >
+          </div>
+        </section>
+        <section class="basic-section">
+          <h2>Pseudoclasses: Add style to even spans or add hover effects</h2>
+          <div id="sub-section-2">
+            <span>Text 1</span>
+            <span>Text 2</span>
+            <span>Text 3</span>
+            <span>Text 4</span>
+            <em>Hover me</em>
+          </div>
+        </section>
+        <section class="basic-section">
+          <h2>Pseudoelements: Add a * before and after elements</h2>
+          <div id="sub-section-2">
+            <p class="pseudo-element">Hola</p>
+            <p class="pseudo-element">Hello</p>
+            <p class="pseudo-element">Bonjour</p>
+          </div>
+        </section>
       </div>
     `;
   }
 }
-customElements.define('view-selectors-2', Selectors2);
+customElements.define("view-selectors-2", Selectors2);
